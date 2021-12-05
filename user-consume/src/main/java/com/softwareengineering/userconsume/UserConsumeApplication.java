26353f498e5e252da8fffcb3f3e814f75c190b2f
f4prime <zzz136454872@163.com>
2021-12-05 16:59:56 +0800

    new user-consume

---
+++ user-consume/src/main/java/com/softwareengineering/userconsume/UserConsumeApplication.java
@@ -0,0 +1,13 @@
+package com.softwareengineering.userconsume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserConsumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserConsumeApplication.class, args);
+    }
+
+}
---
+++ user-consume/src/test/java/com/softwareengineering/userconsume/UserConsumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwareengineering.userconsume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserConsumeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
