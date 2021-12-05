c0b05721d956e8436e18aa4426bbbfdcbc8f98dd
f4prime <zzz136454872@163.com>
2021-12-05 17:48:37 +0800

    userconsume work with eureka

--- user-consume/src/main/java/com/softwareengineering/userconsume/UserConsumeApplication.java
+++ user-consume/src/main/java/com/softwareengineering/userconsume/UserConsumeApplication.java
@@ -2,10 +2,12 @@ package com.softwareengineering.userconsume;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserConsumeApplication {
 
--- user-consume/src/main/java/com/softwareengineering/userconsume/service/UserService.java
+++ user-consume/src/main/java/com/softwareengineering/userconsume/service/UserService.java
@@ -3,7 +3,10 @@ package com.softwareengineering.userconsume.service;
 import com.softwareengineering.userconsume.dao.UserDao;
 import com.softwareengineering.userconsume.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,13 +14,24 @@ import java.util.List;
 @Service
 public class UserService {
     @Autowired
-    private UserDao userDao;
-    public List<User> queryUserByIds(List<Long> ids){
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;// Eureka客户端，可以获取到服务实例信息
+
+    public List<User> queryUserByIds(List<Long> ids) {
         List<User> users = new ArrayList<>();
-        for (Long id : ids) {
-            User user = this.userDao.queryUserById(id);
-            users.add(user);
-        }
+        // String baseUrl = "http://localhost:8081/user/";
+        // 根据服务名称，获取服务实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");
+        // 因为只有一个UserService,因此我们直接get(0)获取
+        ServiceInstance instance = instances.get(0);
+        // 获取ip和端口信息
+        String baseUrl = "http://"+instance.getHost() + ":" + instance.getPort()+"/user/";
+        ids.forEach(id -> {
+            // 我们测试多次查询，
+            users.add(this.restTemplate.getForObject(baseUrl + id, User.class));
+        });
         return users;
     }
 }
