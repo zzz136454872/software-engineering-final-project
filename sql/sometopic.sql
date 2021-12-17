26ca798545a3609f9e93de4f504b8a1513634a12
f4prime <zzz136454872@163.com>
2021-12-17 15:34:53 +0800

    update gitignore

---
+++ sql/sometopic.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO topic (id, teacher_id, student_id, title, difficulty, description, requirement, create_time, update_time) 
+VALUES (1, 2, 1, "hello", "very_hard", "abcd", "grade90+", "2021-12-15 00:00:00", "2021-12-15 18:24:53"); 
+
+INSERT INTO user (id, role, username, PASSWORD, NAME, gender, job_num, student_class, title, major, email, RESUME) 
+VALUES (3, 5, NULL, "test",	"easiy", "nothing","grade95+","2021-12-14 00:00:00","2021-12-15 23:47:15"); 
+        
+INSERT INTO user (id, role, username, PASSWORD, NAME, gender, job_num, student_class, title, major, email, RESUME) 
+VALUES (2, 4, 6, "abcd", "hard", "hello", "nothing", "2021-12-14 18:23:31", "2021-12-15 18:25:16"); 
